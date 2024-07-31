a09484b813e2a9f872f009107ee7fae8dd6c177d
Amit kumar <codes.amitkumar@gmail.com>
2024-08-01 00:56:52 +0530

    margin-top

--- src/Page/Cancel.jsx
+++ src/Page/Cancel.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import cancel from '../assest/assest/cancel.gif'
 
 function Cancel(props) {
     return (
         <div>
-            
+            <img src={cancel} alt="cancel" />
         </div>
     );
 }
