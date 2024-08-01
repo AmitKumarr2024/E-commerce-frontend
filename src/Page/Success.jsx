9a8eabac264d868941422c385fd53d455720408c
Amit kumar <codes.amitkumar@gmail.com>
2024-08-01 16:52:40 +0530

    success page-21

--- src/Page/Success.jsx
+++ src/Page/Success.jsx
@@ -4,8 +4,8 @@ import {Link} from 'react-router-dom'
 
 function Success(props) {
   return (
-    <div className="mt-6 rounded-lg w-full mix-blend-multiply bg-green-50 max-w-md mx-auto flex flex-col justify-center items-center p-4" >
-      <img src={success} className="rounded-full mix-blend-multiply" width={800} height={100} alt="success" />
+    <div className="mt-6 rounded-lg w-full mix-blend-multiply bg-blue-100 max-w-md mx-auto flex flex-col justify-center items-center p-4" >
+      <img src={success} className="rounded-full " width={400} height={100} alt="success" />
       <p className="my-5 text-green-600 font-medium text-lg">Payment Done Successfull</p>
       <Link to={'/order'} className="p-2 m-2 border-2 hover:bg-green-600 hover:text-white border-green-600 text-green-600 bg-white font-bold rounded-md">See Your Order</Link>
     </div>
