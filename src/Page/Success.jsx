9d15182b992f82144d911fa0d94d74a6bd98f8a2
Amit kumar <codes.amitkumar@gmail.com>
2024-08-01 15:32:33 +0530

    success page-3

--- src/Page/Success.jsx
+++ src/Page/Success.jsx
@@ -7,7 +7,7 @@ function Success(props) {
     <div className="mt-6 rounded-lg w-full bg-slate-200 max-w-md mx-auto flex flex-col justify-center items-center p-4" >
       <img src={success} className="mix-blend-multiply" width={150} height={150} alt="success" />
       <p className="text-green-700 font-bold text-xl">Payment Done Successfull</p>
-      <Link to={'/order'} className="p-2 m-2 border-2 hover:bg-white hover:text-green-600 hover:border-green-700 bg-green-600 font-bold rounded-md">See Your Order</Link>
+      <Link to={'/order'} className="p-2 m-2 border-2 hover:bg-green-500 hover:text-white hover:border-green-700 text-green-600 bg-white font-bold rounded-md">See Your Order</Link>
     </div>
   );
 }
