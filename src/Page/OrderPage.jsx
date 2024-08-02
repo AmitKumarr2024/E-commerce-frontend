2b5a93d2c4acd3ed01095fdf99259d2e33a4af1f
Amit kumar <codes.amitkumar@gmail.com>
2024-08-02 11:08:24 +0530

    delete order button-2

--- src/Page/OrderPage.jsx
+++ src/Page/OrderPage.jsx
@@ -47,13 +47,7 @@ function OrderPage(props) {
               <div className="border rounded-lg shadow-sm overflow-hidden">
                 <div className="lg:flex">
                   <div className="grid gap-4 p-4 lg:w-2/3">
-                  {/* delete button below*/}
-                    <div
-                      className="absolute right-0 p-1 mr-2 text-2xl text-red-600 rounded-full hover:bg-red-500 hover:text-white cursor-pointer"
-                      onClick={(e) => deleteCart(product?._id)}
-                    >
-                      <MdDelete />
-                    </div>
+                 
                     {item.productDetails.map((product, index) => {
                       return (
                         <div
@@ -83,6 +77,13 @@ function OrderPage(props) {
                     })}
                   </div>
                   <div className="p-4 lg:w-1/3 bg-gray-50 space-y-4">
+                   {/* delete button below*/}
+                   <div
+                      className="absolute right-0 p-1 mr-2 text-2xl text-red-600 rounded-full hover:bg-red-500 hover:text-white cursor-pointer"
+                      onClick={(e) => deleteCart()}
+                    >
+                      <MdDelete />
+                    </div>
                     <div>
                       <div className="text-lg font-medium text-gray-800">
                         Payment Details:
