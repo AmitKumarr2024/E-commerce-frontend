e0ca9a1c38dd9d24acf75eb1522e9f7c330560f1
Amit kumar <codes.amitkumar@gmail.com>
2024-08-03 18:45:57 +0530

    user css working-40

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        backgroundAnimation: {
+          "0%": { backgroundColor: "red" },
+          "25%": { backgroundColor: "yellow" },
+          "50%": { backgroundColor: "green" },
+          "75%": { backgroundColor: "blue" },
+          "100%": { backgroundColor: "red" },
+        },
+      },
+      animation: {
+        backgroundAnimation: "backgroundAnimation 4s linear infinite",
+      },
+    },
     container: {
       padding: {
-        DEFAULT: '1rem',
-        sm: '2rem',
-        lg: '4rem',
-        xl: '5rem',
-        '2xl': '6rem',
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
       },
     },
   },
   plugins: [],
-}
+};
