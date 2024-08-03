40ff99aa23af9e6c39697fa2fe782472262086ba
Amit kumar <codes.amitkumar@gmail.com>
2024-08-03 14:57:00 +0530

    all order in admin user name-5

--- src/Page/AllOrders.jsx
+++ src/Page/AllOrders.jsx
@@ -89,7 +89,7 @@ function AllOrders(props) {
               {moment(item.createdAt).format("LL")}
             </p>
 
-            <div>{data[0].productDetails[0].name}</div>
+            <div>{data[0].productDetails[0]._id}</div>
 
 
             <div className="border rounded-lg shadow-sm overflow-hidden">
