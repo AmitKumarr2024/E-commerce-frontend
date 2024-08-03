62217f4b455404f34c6914937e563bc5cdf1e531
Amit kumar <codes.amitkumar@gmail.com>
2024-08-03 21:36:35 +0530

    user css working-64

--- src/Page/AllOrders.jsx
+++ src/Page/AllOrders.jsx
@@ -87,8 +87,8 @@ function AllOrders(props) {
       )}
       <div className="space-y-6 border p-9 rounded-lg shadow-lg overflow-hidden bg-gray-100">
         {user && (
-          <div className="text-center border-2  rounded-md  mb-4 py-1 flex flex-col md:flex-row items-center justify-evenly shadow-lg animated-border">
-          <h2 className="w-auto transition-all text-xl font-semibold animate-backgroundAnimation text-white px-4 rounded-2xl shadow-md mb-4 md:mb-0 mx-auto">
+          <div className="text-center border-2  rounded-md   py-1 flex flex-col md:flex-row items-center justify-evenly shadow-lg animated-border">
+          <h2 className="w-auto transition-all text-xl font-semibold animate-backgroundAnimation text-white px-10 rounded-2xl shadow-md mb-4 md:mb-0 mx-auto">
             User Details
           </h2>
           <div className="mt-4 md:mt-0 flex flex-col md:flex-row justify-evenly w-full items-center space-y-2 md:space-y-0 md:space-x-4">
